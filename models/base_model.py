4c983f015ed3d6ab27402a5b187db9baa852404e
“Dino” <daringamaleldin@hotmail.com>
2024-02-10 12:19:33 -0800

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,15 @@ from uuid import uuid4
 from datatime import datetime
 
 class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -24,13 +32,20 @@ class BaseModel:
             self.updated_at = datetime.now()
 
 def __str__(self):
+    """Return the print/str representation of the BaseModel instance."""
     return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
 def save(self):
+    """Update updated_at with the current datetime."""
     self.updated_at = datetime.now()
 
 
 def to_dict(self):
+    """Return the dictionary of the BaseModel instance.
+
+    Includes the key/value pair __class__ representing
+    the class name of the object.
+    """
     model_dict = self.__dict__.copy()
     model_dict['__class__'] = self.__class__.__name__
     model_dict['created_at'] = self.created_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,21 +2,36 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
+    """Represent an abstracted storage engine.
+
+    Attributes:
+       __file_path (str): The name of the file to save objects to.
+       __objects (dict): A dictionary of instantiated objects.
+    """
     __file_path = "file.json"
     __objects = {}
 
 
     def all(self):
+        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """Serialize __objects to the JSON file __file_path."""
         serialized_objects = {}
         for key, obj in self.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -25,6 +40,7 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(self.__file_path, 'r') as file:
                 serialized_objects = json.load(file)
@@ -35,4 +51,4 @@ class FileStorage:
                     self.__objects[key] = obj
 
         except FileNotFoundError:
-            pass
+            return
