09799df4fa0d67013c9d5ecc74643e0fa9c57dfc
“Dino” <daringamaleldin@hotmail.com>
2024-02-11 10:53:24 -0800

    update

--- models/place.py
+++ models/place.py
@@ -4,39 +4,38 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represent a place.
+"""Represent a place.
 
-    Attributes:
-        city_id (str): The City id.
-        user_id (str): The User id.
-        name (str): The name of the place.
-        description (str): The description of the place.
-        number_rooms (int): The number of rooms of the place.
-        number_bathrooms (int): The number of bathrooms of the place.
-        max_guest (int): The maximum number of guests of the place.
-        price_by_night (int): The price by night of the place.
-        latitude (float): The latitude of the place.
-        longitude (float): The longitude of the place.
-        amenity_ids (list): A list of Amenity ids.
-    """
+Attributes:
+city_id (str): The City id.
+user_id (str): The User id.
+name (str): The name of the place.
+description (str): The description of the place.
+number_rooms (int): The number of rooms of the place.
+number_bathrooms (int): The number of bathrooms of the place.
+max_guest (int): The maximum number of guests of the place.
+price_by_night (int): The price by night of the place.
+latitude (float): The latitude of the place.
+longitude (float): The longitude of the place.
+amenity_ids (list): A list of Amenity ids.
+"""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a new Place instance.
+def __init__(self, *args, **kwargs):
+"""Initialize a new Place instance.
 
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        super().__init__(*args, **kwargs)
-        self.city_id = kwargs.get('city_id', '')
-        self.user_id = kwargs.get('user_id', '')
-        self.name = kwargs.get('name', '')
-        self.description = kwargs.get('description', '')
-        self.number_rooms = kwargs.get('number_rooms', 0)
-        self.number_bathrooms = kwargs.get('number_bathrooms', 0)
-        self.max_guest = kwargs.get('max_guest', 0)
-        self.price_by_night = kwargs.get('price_by_night', 0)
-        self.latitude = kwargs.get('latitude', 0.0)
-        self.longitude = kwargs.get('longitude', 0.0)
-        self.amenity_ids = kwargs.get('amenity_ids', [])
-
+Args:
+*args (any): Unused.
+**kwargs (dict): Key/value pairs of attributes.
+"""
+super().__init__(*args, **kwargs)
+self.city_id = kwargs.get('city_id', '')
+self.user_id = kwargs.get('user_id', '')
+self.name = kwargs.get('name', '')
+self.description = kwargs.get('description', '')
+self.number_rooms = kwargs.get('number_rooms', 0)
+self.number_bathrooms = kwargs.get('number_bathrooms', 0)
+self.max_guest = kwargs.get('max_guest', 0)
+self.price_by_night = kwargs.get('price_by_night', 0)
+self.latitude = kwargs.get('latitude', 0.0)
+self.longitude = kwargs.get('longitude', 0.0)
+self.amenity_ids = kwargs.get('amenity_ids', [])
