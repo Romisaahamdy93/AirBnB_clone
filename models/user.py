79ee54fad7c260bcb3263d79b892f58e6e531934
Romisaahamdy93 <romisaa.hamdy@yahoo.com>
2024-02-11 10:10:05 -0800

    edit

--- models/user.py
+++ models/user.py
@@ -1,8 +1,23 @@
+#!/usr/bin/python3
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
-class User(BaseModel):
-    """User class"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class Review(BaseModel):
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Review instance."""
+        super().__init__(*args, **kwargs)
+        self.place_id = ""
+        self.user_id = ""
+        self.text = ""
+
+    def __str__(self):
+        """Return the string representation of Review."""
+        return "[Review] ({}) {}".format(self.id, self.to_dict())
