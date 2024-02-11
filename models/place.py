331bfa8d70edca96105f85abcdadf9a736d931b2
“Dino” <daringamaleldin@hotmail.com>
2024-02-11 09:40:03 -0800

    update

--- models/place.py
+++ models/place.py
@@ -4,23 +4,39 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represents a Place for the HBnB project."""
+    """Represent a place.
+
+    Attributes:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
+
     def __init__(self, *args, **kwargs):
-        """Initialize a new Place."""
+        """Initialize a new Place instance.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
         super().__init__(*args, **kwargs)
-        self.city_id = ""
-        self.user_id = ""
-        self.name = ""
-        self.description = ""
-        self.number_rooms = 0
-        self.number_bathrooms = 0
-        self.max_guest = 0
-        self.price_by_night = 0
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.amenity_ids = []
+        self.city_id = kwargs.get('city_id', "")
+        self.user_id = kwargs.get('user_id', "")
+        self.name = kwargs.get('name', "")
+        self.description = kwargs.get('description', "")
+        self.number_rooms = kwargs.get('number_rooms', 0)
+        self.number_bathrooms = kwargs.get('number_bathrooms', 0)
+        self.max_guest = kwargs.get('max_guest', 0)
+        self.price_by_night = kwargs.get('price_by_night', 0)
+        self.latitude = kwargs.get('latitude', 0.0)
+        self.longitude = kwargs.get('longitude', 0.0)
+        self.amenity_ids = kwargs.get('amenity_ids', [])
 
-
-    def __str__(self):
-        """Return the print/str representation of the Place instance."""
-        return "[Place] ({}) {}".format(self.id, self.__dict__)
